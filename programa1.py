ee40c80d6230b444881971d8c8df92e87fa9a093
vicky-a913 <a385983@uach.mx>
2025-10-01 08:26:26 -0700

    Update programa1.py

--- programa1.py
+++ programa1.py
@@ -1,5 +1,5 @@
 # Este es un script de python de prueba para el repositorio.
 def saludo(nombre):
    return "Hola" + nombre
-# Se invoca a la función saludo 
+# Se invoca a la función saludo para saludar a alguien llamado Carlos.
 saludo("Carlos")
